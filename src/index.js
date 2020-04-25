1a143f44f07953eb9532f60a3c76708169554f94
Vassiliy278 <trafaret2009@gmail.com>
2020-04-25 16:58:57 +0300

    easy html

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import message from './message'
+import arrow from './arrow'
+import './style.css'
+
+console.log(arrow(5,10))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 console.log(`dirname: ${__dirname}`)
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'] //работает справа налево
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: true,
+        }),
+        new CleanWebpackPlugin(),
+        
+    ]
     }
-}
+
